5ba739d1c0bfc0ef7d53c08ac712784fbbd54c88
camilogarzonrojas <ce.garzon12@uniandes.edu.co>
2018-11-18 17:04:31 -0500

    avance ODE

--- ODE.cpp
+++ ODE.cpp
@@ -11,6 +11,12 @@ float a_arr(float magnitud, float v_x_y)
 
 int main()
 {
-
+	float a1x, a1y, a2x, a2y, a3x, a3y, a4x, a4y, b1x, b1y, b2x, b2y, b3x, b3y, b4x, b4y, x, y, vx, vy, t, theta, v;
+	theta=3.1416/4;
+	vx=300*cos(theta);
+	vy=300*cos(theta);
+	x=0;
+	y=0;
+	t=0.001;
 	return 0;
 }
