6132ba8272e2faa371fe3230149965544acf4e4a
camilogarzonrojas <ce.garzon12@uniandes.edu.co>
2018-11-18 23:35:41 -0500

    avance ODE

--- ODE.cpp
+++ ODE.cpp
@@ -18,5 +18,39 @@ int main()
 	x=0;
 	y=0;
 	t=0.001;
+	//Se hace un bucle llamado do para ejecutar el método por runge kutta:
+	do
+	{
+		//Se calculan los coeficientes de las velocidades
+		
+		v=sqrt(pow(vx,2)+pow(vy,2)); //En un prinicpio se obtiene la magnitud de v
+		b1x=a_arr(v,vx);
+		b1y=-10+a_arr(v,vy);
+		v=sqrt(pow(vx+b1x*t/2,2)+pow(vy+b1y*t/2,2));
+		b2x=a_arr(v,vx+b1x*t/2);
+		b2y=-10+a_arr(v,vy+b1y*t/2);
+		v=sqrt(pow(vx+b2x*t/2,2)+pow(vy+b2y*t/2,2));
+		b3x=a_arr(v,vx+b2x*t/2);
+		b3y=-10+a_arr(v,vy+b2y*t/2);
+		v=sqrt(pow(vx+b3x*t/2,2)+pow(vy+b3y*t/2,2));
+		b4x=a_arr(v,vx+b3x*t/2);
+		b4y=-10+a_arr(v,vy+b3y*t/2);
+
+		a1x=vx;
+		a1y=vy; //Se calculan los coeficientes de x y y
+		a2x=vx+b1x*t/2;
+		a2y=vy+b1y*t/2;
+		a3x=vx+b2x*t/2;
+		a3y=vy+b2y*t/2;
+		a4x=vx+b3x*t/2;
+		a4y=vy+b3y*t/2;
+
+		x=x+(t/6)*(a1x+2*a2x+2*a3x+a4x);
+		y=y+(t/6)*(a1y+2*a2y+2*a3y+a4y);
+		vx=vx+(t/6)*(b1x+2*b2x+2*b3x+b4x);
+		vy=vy+(t/6)*(b1y+2*b2y+2*b3y+b4y); //Se actualizan los valores de x, y, vx y vy
+		cout<<x<<", "<<y<<"\n";
+		
+	}
 	return 0;
 }
